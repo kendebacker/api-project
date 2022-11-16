8cddafd589002cc1b08aab46b9f18193746db27b
Ken DeBacker <kcdebacker@gmail.com>
2022-11-16 11:50:52 -0500

    commit

--- index.js
+++ index.js
@@ -13,80 +13,70 @@ let memory = ``
 
 
 app.listen(PORT,  ()=> {
-        memory =  fs.readFileSync("./Storage/memory.txt",  'utf-8')
-        const midway = memory.replace(/ /g,"").split("$:$").map(el => el.split("$?"))
-        midway.forEach((el,ind)=> memoryStorage.set(el[0], {name: el[0], duration: parseInt(el[2]), added: el[1], path: `./Storage/${el[0]}`}))
-        console.log('Running...')}
-)
-
+    // Create map of existing files already present with basic information for querying
+    memory =  fs.readFileSync("./Storage/memory.txt",  'utf-8')
+    const midway = memory.replace(/ /g,"").split("$:$").map(el => el.split("$?"))
+    midway.forEach((el,ind)=> memoryStorage.set(el[0], 
+        {name: el[0], duration: parseInt(el[2]), added: el[1], path: `./Storage/${el[0]}`}))
+    console.log('Running...')
+})
 
 
 const storage = multer.diskStorage({
-    filename: function (req, file, callback) {
-        if(memoryStorage.get(file.originalname)===undefined){
-            callback(null, file.originalname)
-        }else{
-            callback(null, "discard.wav")
-        }
+    // If file already exists save to discard, if not save to name
+    filename: (req, file, callback)=>{
+        callback(null, file.originalname)
     },
-    destination: function (req, file, callback) {
-            callback(null, './Storage')
+    destination: (req, file, callback)=>{
+        callback(null, "./Storage")
     }
 })
+
+const fileFilter =(req, file, callback) =>{
+    callback(null, memoryStorage.get(file.originalname)===undefined)
+}
   
-const upload = multer({ storage })
+const upload = multer({fileFilter, storage })
 
 app.get(`/list`, (req,res)=>{
-
+    // Query map based on duration, return array of names
     const maxDur = req.query.maxduration
-    let songs = Array.from(memoryStorage.values())
-    console.log(songs)
-    songs = songs.filter(song => song.duration <=  maxDur).map(song => song.name)
-
-    console.log(songs)
-    console.log(maxDur)
-    const response = songs.length ===0?
-    "Sorry, no files found":`Found ${songs.length} files that match requirments`
-
-    res.json({response: response, data: songs})
-
+    let files = Array.from(memoryStorage.values())
+    files = files.filter(file => file.duration <=  maxDur).map(file => file.name)
+    const response = files.length ===0?"No files found":`Found ${files.length} files`
+    res.json({response: response, data: files})
 })
 
 app.get(`/info`, (req,res)=>{
-
-    const response = memoryStorage.get(req.query.name)===undefined?
-    `Could not find file with name ${req.query.name}`:`The info for file ${req.query.name}`
-
-    const fileInfo = memoryStorage.get(req.query.name)===undefined?
-    "":memoryStorage.get(req.query.name)
-
+    // Query map based on key name (file name), return file object
+    const fileName = req.query.name
+    const response = memoryStorage.get(fileName)===undefined?`No info for ${fileName}`:`Info for ${fileName}`
+    const fileInfo = memoryStorage.get(fileName)===undefined?"":memoryStorage.get(fileName)
     res.json({response: response, data: fileInfo})
-
 })
 
 app.get(`/download`, (req,res)=>{
-    if(memoryStorage.get(req.query.name)!==undefined){
-        res.download(`Storage/${req.query.name}`, req.query.name)
+    // See if file present in map, if so download if not throw error.
+    const fileName = req.query.name
+    if(memoryStorage.get(fileName)!==undefined){
+        res.download(`Storage/${fileName}`, fileName)
     }else{
-        res.status(404).send(`${req.query.name} not found`)
+        res.status(404).send(`${fileName} not found`)
     }
 })
 
 app.post("/post", upload.single("file"), (req, res)=>{
-        const fileName = req.file.originalname
-
-        const response = memoryStorage.get(fileName)===undefined?
-        `${fileName} recieved and stored`:`${fileName} already exists, please select a unique name`
-
+    // Check if song present, if so don't store in map, if not store in map
+    if(req.file !== undefined){
         getAudioDurationInSeconds(req.file.path).then(dur => {
-        const newPeice = memoryStorage.get(fileName)===undefined?
-        `${fileName}$?${Date.now()}$?${dur}`:""
-
-        memoryStorage.set(fileName,memoryStorage.get(fileName)===undefined?
-        {name: fileName, duration: dur, added: Date.now(), path: req.file.path}:memoryStorage.get(fileName))
-        memory = `${memory}${newPeice}$:$`
-        fs.writeFileSync("./Storage/memory.txt", memory)
-        res.json({response: response})
+            const fileName = req.file.originalname
+            const newPeice = `${fileName}$?${Date.now()}$?${dur}`
+            memoryStorage.set(fileName,{name: fileName, duration: dur, added: Date.now(), path: req.file.path})
+            memory = `${memory}${newPeice}$:$`
+            fs.writeFileSync("./Storage/memory.txt", memory)
         })
+
+    }
+    res.json({response: req.file === undefined?"File already exists":"File stored"})
 })
 
