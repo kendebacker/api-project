a29589330c0c9eaa253530f41605f480d13a2fb0
Ken Debacker <kcdebacker@gmail.com>
2023-07-16 14:45:28 -0700

    chore: ran prettier

--- index.js
+++ index.js
@@ -1,90 +1,124 @@
 const express = require("express");
-const multer = require('multer');
-const cors = require('cors')
-const fs = require('fs')
-const PORT = 8080
-const app = express()
-const { getAudioDurationInSeconds } = require('get-audio-duration')
-app.use(cors())
+const multer = require("multer");
+const cors = require("cors");
+const fs = require("fs");
+const PORT = 8080;
+const app = express();
+const { getAudioDurationInSeconds } = require("get-audio-duration");
+app.use(cors());
 
-let mapMemory = null
+let mapMemory = null;
 
-app.listen(PORT,  async ()=> {
-    // Create map of existing files already present with basic information for querying
-    fs.readFile("./storage/memory.json",  'utf8', (err, data) =>{
-        if(err){
-            console.log(err)
-        }
-        mapMemory = data===""?new Map():new Map(Object.entries(JSON.parse(data)));
-        console.log("Running...")
-    })
-})
+app.listen(PORT, async () => {
+  // Create map of existing files already present with basic information for querying
+  fs.readFile("./storage/memory.json", "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    mapMemory =
+      data === "" ? new Map() : new Map(Object.entries(JSON.parse(data)));
+    console.log("Running...");
+  });
+});
 
 const storage = multer.diskStorage({
-    // Save file to Storage folder
-    filename: (req, file, callback)=>{
-        callback(null, file.originalname)
-    },
-    destination: (req, file, callback)=>{
-        callback(null, "./storage")
+  // Save file to Storage folder
+  filename: (req, file, callback) => {
+    callback(null, file.originalname);
+  },
+  destination: (req, file, callback) => {
+    callback(null, "./storage");
+  },
+});
+
+const fileFilter = (req, file, callback) => {
+  // Check if file present already/ correct extension
+  const extenstion = file.originalname.split(".");
+  callback(
+    null,
+    mapMemory.get(file.originalname) === undefined &&
+      extenstion.length == 2 &&
+      extenstion[1] === "wav",
+  );
+};
+
+const upload = multer({
+  fileFilter: fileFilter,
+  storage: storage,
+  limits: { fileSize: 3000000 },
+}).single("file");
+
+app.post("/post", (req, res) => {
+  // check if file meets standards/is already present
+  upload(req, res, (err) => {
+    if (err || req.file === undefined) {
+      res
+        .status(400)
+        .json({
+          body: `File already exists, is to large, or is improperly formated`,
+        });
+    } else {
+      getAudioDurationInSeconds(req.file.path)
+        .then((dur) => {
+          const currentDay = new Date().toLocaleDateString();
+          const fileName = req.file.originalname;
+          mapMemory.set(fileName, {
+            name: fileName,
+            duration: dur,
+            dateAdded: currentDay,
+            path: req.file.path,
+          });
+          fs.writeFile(
+            "./storage/memory.json",
+            JSON.stringify(Object.fromEntries(mapMemory)),
+            (err) => {
+              res
+                .status(err ? 500 : 200)
+                .json({
+                  body: err
+                    ? `Error storing data: ${err}`
+                    : `${fileName} succesfully stored`,
+                });
+            },
+          );
+        })
+        .catch((err) =>
+          res.status(500).json({ body: `Error getting duration: ${err}` }),
+        );
     }
-})
+  });
+});
 
-const fileFilter =(req, file, callback) =>{
-    // Check if file present already/ correct extension
-    const extenstion = file.originalname.split(".")
-    callback(null, mapMemory.get(file.originalname)===undefined && extenstion.length==2 && extenstion[1]==="wav")
-}
-  
-const upload = multer({fileFilter: fileFilter, storage: storage, limits: {fileSize: 3000000}}).single("file")
+app.get(`/list`, (req, res) => {
+  // Query map based on duration, return array of names
+  const maxDur = req.query.maxduration;
+  if (!isNaN(maxDur) && !isNaN(parseFloat(maxDur))) {
+    const files = Array.from(mapMemory.values())
+      .filter((file) => file.duration <= parseFloat(maxDur))
+      .map((file) => file.name);
+    const response =
+      files.length === 0 ? "No files found" : `Found ${files.length} files`;
+    res.status(200).json({ body: response, data: files });
+  } else {
+    res.status(400).json({ body: "Query is not properly formatted", data: [] });
+  }
+});
 
-app.post("/post", (req, res)=>{
-    // check if file meets standards/is already present
-    upload(req, res, (err)=>{
-        if(err || req.file === undefined){
-            res.status(400).json({body: `File already exists, is to large, or is improperly formated`})
-        }else{
-            getAudioDurationInSeconds(req.file.path).then(dur => {
-                const currentDay = new Date().toLocaleDateString()
-                const fileName = req.file.originalname
-                mapMemory.set(fileName, {name: fileName, duration: dur, dateAdded: currentDay, path: req.file.path})
-                fs.writeFile("./storage/memory.json", JSON.stringify(Object.fromEntries(mapMemory)), (err)=>{
-                    res.status(err?500:200).json({body: err ?`Error storing data: ${err}`:`${fileName} succesfully stored`})
-                })
-            }).catch((err) => res.status(500).json({body: `Error getting duration: ${err}`}))
-        }
-    })
-})
+app.get(`/info`, (req, res) => {
+  // Query map based on key name (file name), return file object
+  const fileName = req.query.name;
+  const noFile = mapMemory.get(fileName) === undefined;
+  const response = noFile ? `No file info found` : `File info retrieved`;
+  const fileInfo = noFile ? "" : mapMemory.get(fileName);
+  res.status(noFile ? 400 : 200).json({ body: response, data: fileInfo });
+});
 
-app.get(`/list`, (req,res)=>{
-    // Query map based on duration, return array of names
-    const maxDur = req.query.maxduration
-    if(!isNaN(maxDur) && !isNaN(parseFloat(maxDur))){
-        const files = Array.from(mapMemory.values()).filter(file => file.duration <=  parseFloat(maxDur)).map(file => file.name)
-        const response = files.length ===0?"No files found":`Found ${files.length} files`
-        res.status(200).json({body: response, data: files})
-    }else{
-        res.status(400).json({body: "Query is not properly formatted", data: []})
-    }
-
-})
-
-app.get(`/info`, (req,res)=>{
-    // Query map based on key name (file name), return file object
-    const fileName  = req.query.name
-    const noFile = mapMemory.get(fileName)===undefined
-    const response = noFile?`No file info found`:`File info retrieved`
-    const fileInfo = noFile?"":mapMemory.get(fileName)
-    res.status(noFile?400:200).json({body: response, data: fileInfo})
-})
-
-app.get(`/download`, (req,res)=>{
-    // See if file present in map, if so download if not throw error.
-    const fileName = req.query.name
-    if(mapMemory.get(fileName)!==undefined){
-        res.status(200).download(`${mapMemory.get(fileName).path}`, fileName)
-    }else{
-        res.status(400).json({body: "File not found"})
-    }
-})
-
+app.get(`/download`, (req, res) => {
+  // See if file present in map, if so download if not throw error.
+  const fileName = req.query.name;
+  if (mapMemory.get(fileName) !== undefined) {
+    res.status(200).download(`${mapMemory.get(fileName).path}`, fileName);
+  } else {
+    res.status(400).json({ body: "File not found" });
+  }
+});
--- src/App.js
+++ src/App.js
@@ -1,79 +1,153 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [file, setFile] = useState("");
+  const [response, setResponse] = useState("");
+  const [searchName, setSearchName] = useState("");
+  const [searchDuration, setSearchDuration] = useState("");
+  const [nameQuery, setNameQuery] = useState("");
+  const [durationQuery, setDurationQuery] = useState("");
+  const [searchDataName, setSearchDataName] = useState("");
+  const [searchData, setSearchData] = useState("");
 
-  const [file, setFile] = useState("")
-  const [response, setResponse] = useState("")
-  const [searchName, setSearchName] = useState("")
-  const [searchDuration, setSearchDuration] = useState("")
-  const [nameQuery, setNameQuery] = useState("")
-  const [durationQuery, setDurationQuery] = useState("")
-  const [searchDataName, setSearchDataName] = useState("")
-  const [searchData, setSearchData] = useState("")
-  
-  const uploadAudio = ()=>{
-    const formData = new FormData()
-    formData.append("file", file)
-    const data = {method: 'POST', body: formData, }
-    fetch('http://localhost:8080/post', data)
-    .then(res => res.json())
-    .then(res => setResponse(res))
-    .catch(err=> alert(err))
-  }
+  const uploadAudio = () => {
+    const formData = new FormData();
+    formData.append("file", file);
+    const data = { method: "POST", body: formData };
+    fetch("http://localhost:8080/post", data)
+      .then((res) => res.json())
+      .then((res) => setResponse(res))
+      .catch((err) => alert(err));
+  };
 
-  const queryName=()=>{
+  const queryName = () => {
     fetch(`http://localhost:8080/info?name=${searchName}`)
-    .then(res => res.json())
-    .then(res => setNameQuery(res))
-    .catch(err=> alert(err))
-  }
+      .then((res) => res.json())
+      .then((res) => setNameQuery(res))
+      .catch((err) => alert(err));
+  };
 
-  const queryDuration=()=>{
+  const queryDuration = () => {
     fetch(`http://localhost:8080/list?maxduration=${searchDuration}`)
-    .then(res => res.json())
-    .then(res => setDurationQuery(res))
-    .catch(err=> alert(err))
-  }
+      .then((res) => res.json())
+      .then((res) => setDurationQuery(res))
+      .catch((err) => alert(err));
+  };
 
-  const queryData=()=>{
+  const queryData = () => {
     fetch(`http://localhost:8080/download?name=${searchDataName}`)
-    .then(res => {
-      if(res.ok){
-        return res.blob()
-      }else{
-        throw new Error("File not found")
-      }
-    }).then(res => {
-      setSearchData(`${searchDataName} downloading`)
-      const el = document.createElement("a")
-      el.href = URL.createObjectURL(res)
-      el.download = `${searchDataName}`
-      document.body.appendChild(el)
-      el.click()
-    }).catch((err)=> alert(err))
-  }
+      .then((res) => {
+        if (res.ok) {
+          return res.blob();
+        } else {
+          throw new Error("File not found");
+        }
+      })
+      .then((res) => {
+        setSearchData(`${searchDataName} downloading`);
+        const el = document.createElement("a");
+        el.href = URL.createObjectURL(res);
+        el.download = `${searchDataName}`;
+        document.body.appendChild(el);
+        el.click();
+      })
+      .catch((err) => alert(err));
+  };
 
   return (
     <div className="App">
-        <h2>Search file Info</h2>
-        <input  onChange={(e)=>{setSearchName(e.target.value)}} type={"text"} name={"fileName"}></input><br/>
-        <button onClick={()=>{queryName()}}>Search</button><br/>
-        <strong>{nameQuery===""?"":nameQuery.body}</strong>
-        <ul>{nameQuery==="" || nameQuery.data===""?"":Object.keys(nameQuery.data).map((el,ind)=><li key={ind}>{el}: {nameQuery.data[el]}</li>)}</ul>
-        <h2>Search Max Duration</h2>
-        <input  onChange={(e)=>{setSearchDuration(e.target.value)}} type={"number"} name={"duration"}></input><br/>
-        <button onClick={()=>{queryDuration()}}>Search</button><br/>
-        <strong>{durationQuery===""?"":durationQuery.body}</strong>
-        {durationQuery===""?"":<ol>{durationQuery.data.map((el,ind)=> <li key={ind}>{el}</li>)}</ol>}
-        <h2>Upload File:</h2>
-        <input  onChange={(e)=>{setFile(e.target.files[0])}} type={"file"} name={"file"}></input><br/>
-        <button disabled={file===""?true:false} onClick={()=>{uploadAudio()}}>Submit File</button><br/>
-        <strong>{response===""?"":response.body}</strong>
-        <h2>Search for file Data</h2>
-        <input  onChange={(e)=>{setSearchDataName(e.target.value)}}  type={"text"} name={"fileName"}></input><br/>
-        <button onClick={()=>{queryData()}}>Search</button><br/>
-        <strong>{searchData===""?"":searchData}</strong>
+      <h2>Search file Info</h2>
+      <input
+        onChange={(e) => {
+          setSearchName(e.target.value);
+        }}
+        type={"text"}
+        name={"fileName"}
+      ></input>
+      <br />
+      <button
+        onClick={() => {
+          queryName();
+        }}
+      >
+        Search
+      </button>
+      <br />
+      <strong>{nameQuery === "" ? "" : nameQuery.body}</strong>
+      <ul>
+        {nameQuery === "" || nameQuery.data === ""
+          ? ""
+          : Object.keys(nameQuery.data).map((el, ind) => (
+              <li key={ind}>
+                {el}: {nameQuery.data[el]}
+              </li>
+            ))}
+      </ul>
+      <h2>Search Max Duration</h2>
+      <input
+        onChange={(e) => {
+          setSearchDuration(e.target.value);
+        }}
+        type={"number"}
+        name={"duration"}
+      ></input>
+      <br />
+      <button
+        onClick={() => {
+          queryDuration();
+        }}
+      >
+        Search
+      </button>
+      <br />
+      <strong>{durationQuery === "" ? "" : durationQuery.body}</strong>
+      {durationQuery === "" ? (
+        ""
+      ) : (
+        <ol>
+          {durationQuery.data.map((el, ind) => (
+            <li key={ind}>{el}</li>
+          ))}
+        </ol>
+      )}
+      <h2>Upload File:</h2>
+      <input
+        onChange={(e) => {
+          setFile(e.target.files[0]);
+        }}
+        type={"file"}
+        name={"file"}
+      ></input>
+      <br />
+      <button
+        disabled={file === "" ? true : false}
+        onClick={() => {
+          uploadAudio();
+        }}
+      >
+        Submit File
+      </button>
+      <br />
+      <strong>{response === "" ? "" : response.body}</strong>
+      <h2>Search for file Data</h2>
+      <input
+        onChange={(e) => {
+          setSearchDataName(e.target.value);
+        }}
+        type={"text"}
+        name={"fileName"}
+      ></input>
+      <br />
+      <button
+        onClick={() => {
+          queryData();
+        }}
+      >
+        Search
+      </button>
+      <br />
+      <strong>{searchData === "" ? "" : searchData}</strong>
     </div>
   );
 }
